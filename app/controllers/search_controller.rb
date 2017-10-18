bbc434baae6f303251f22417cf263cc137b74c59
erose357 <erose357@gmail.com>
2017-10-18 10:08:49 -0600

    Beginning to build Faraday connection, took longer than expected to find necessary info in api docs

--- app/controllers/search_controller.rb
+++ app/controllers/search_controller.rb
@@ -1,4 +1,8 @@
 class SearchController < ApplicationController
   def index
+    @conn = Faraday.new(url: https://api.data.gov/nrel/alt-fuel-stations/v1/nearest.json) do |faraday|
+      faraday.headers['X-Api-Key'] = ENV['nrel_api_key']
+      faraday.adapter Faraday.default_adapter
+    end
   end
 end
