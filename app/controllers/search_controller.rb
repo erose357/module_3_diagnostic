99b621f163857b816eca4d7821e6af9d14aaed60
erose357 <erose357@gmail.com>
2017-10-18 11:02:31 -0600

    Working on sending api data to view

--- app/controllers/search_controller.rb
+++ app/controllers/search_controller.rb
@@ -7,7 +7,26 @@ class SearchController < ApplicationController
     
     response = Faraday.get("https://api.data.gov/nrel/alt-fuel-stations/v1/nearest.json?api_key=#{ENV['nrel_api_key']}&fuel_type=ELEC,LPG&location=80203")
     
-    stations = JSON.parse(response.body, symbolize_names: true)
-    binding.pry
+    raw_stations = JSON.parse(response.body, symbolize_names: true)[:fuel_stations]
+
+    @stations = raw_stations.map do |raw_station|
+      Station.new(raw_station)
+    end
+
+    
+  end
+end
+
+class Station
+  attr_reader :name, :street_address, :state, :zip, :fuel_types, :distance, :access_times
+  
+  def initialize(attrs)
+    @name = attrs[:station_name]
+    @street_address = attrs[:street_address]
+    @state = attrs[:state]
+    @zip = attrs[:zip]
+    @fuel_types = attrs[:fuel_type_code]
+    @distance = attrs[:distance]
+    @access_times = attrs[:access_days_time]
   end
 end
--- spec/features/user_can_search_for_stations_spec.rb
+++ spec/features/user_can_search_for_stations_spec.rb
@@ -13,7 +13,7 @@ RSpec.feature "User can search" do
   #Then I should be on page "/search" with parameters visible in the url
       expect(current_path).to eq("/search")
   #Then I should see a list of the 10 closest stations within 6 miles sorted by distance
-      expect(page).to have_css('li.station', count: 10)
+      expect(page).to have_css('h3.station', count: 10)
   #And the stations should be limited to Electric and Propane
   #And for each of the stations I should see Name, Address, Fuel Types, Distance, and Access Times
       expect(page).to have_css('li.name', count: 10)
