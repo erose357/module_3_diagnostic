0db3c6c10137cfe230f306e58a2f2b0eb7881830
erose357 <erose357@gmail.com>
2017-10-18 10:20:50 -0600

    Adds VCR, setting up api call with faraday

--- app/controllers/search_controller.rb
+++ app/controllers/search_controller.rb
@@ -1,8 +1,12 @@
 class SearchController < ApplicationController
   def index
-    @conn = Faraday.new(url: https://api.data.gov/nrel/alt-fuel-stations/v1/nearest.json) do |faraday|
+    @conn = Faraday.new(url: 'https://api.data.gov/nrel/alt-fuel-stations/v1') do |faraday|
       faraday.headers['X-Api-Key'] = ENV['nrel_api_key']
       faraday.adapter Faraday.default_adapter
     end
+    
+    response = @conn.get('/nearest.json?fuel_type=ELEC,LPG&location=80203')
+    binding.pry
+    stations = JSON.parse(response.body, symbolize_names: true)
   end
 end
