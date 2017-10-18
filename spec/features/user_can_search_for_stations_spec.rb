dd710f11112311c760d940dadc6e931a13fa2648
erose357 <erose357@gmail.com>
2017-10-18 09:44:17 -0600

    Adds search_controller, index view, fixes test expectations

--- spec/features/user_can_search_for_stations_spec.rb
+++ spec/features/user_can_search_for_stations_spec.rb
@@ -6,11 +6,11 @@ RSpec.feature "User can search" do
 #When I visit "/"
     visit "/"
 #And I fill in the search form with 80203
-    fill_in with: '80203'
+    fill_in 'q', with: '80203'
 #And I click "Locate"
     click_on "Locate"
 #Then I should be on page "/search" with parameters visible in the url
-    expect(page).to be("/search")
+    expect(current_path).to eq("/search")
 #Then I should see a list of the 10 closest stations within 6 miles sorted by distance
     expect(page).to have_css('li.station', count: 10)
 #And the stations should be limited to Electric and Propane
