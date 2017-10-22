a711171b7766d09b48535636554fbb063e95a307
erose357 <erose357@gmail.com>
2017-10-22 17:10:34 -0600

    Replaces faraday with rest client

--- app/controllers/search_controller.rb
+++ app/controllers/search_controller.rb
@@ -1,18 +1,18 @@
 class SearchController < ApplicationController
   def index
-    #@conn = Faraday.new(:url => 'https://api.data.gov/nrel/alt-fuel-stations/v1') do |faraday|
-      #faraday.headers['X-Api-Key'] = ENV['nrel_api_key']
-      #faraday.adapter Faraday.default_adapter
-    #end
-    
-    response = Faraday.get("https://api.data.gov/nrel/alt-fuel-stations/v1/nearest.json?api_key=#{ENV['nrel_api_key']}&fuel_type=ELEC,LPG&location=#{params[:q]}&limit=10")
+    params_hash = {fuel_type: 'ELEC,LPG',
+                   location: "#{params[:q]}",
+                   limit: 10,
+                   radius: 6}
+
+    conn = RestClient::Resource.new("https://api.data.gov/nrel/alt-fuel-stations/v1", :headers => {'X-Api-Key' => ENV['nrel_api_key'], :params => params_hash})
+
+    response = conn['/nearest.json'].get
 
     raw_stations = JSON.parse(response.body, symbolize_names: true)[:fuel_stations]
 
     @stations = raw_stations.map do |raw_station|
       Station.new(raw_station)
     end
-
-    
   end
 end
