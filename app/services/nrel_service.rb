4c059e20419cd8d7d8fd0df1fb6e16d5b7357b36
erose357 <erose357@gmail.com>
2017-10-22 18:13:18 -0600

    Refactors nrel_service

--- app/services/nrel_service.rb
+++ app/services/nrel_service.rb
@@ -1,18 +1,22 @@
 class NrelService
-  def fuel_stations(zip)
-    params_hash = {fuel_type: 'ELEC,LPG',
-                   location: zip,
-                   limit: 10,
-                   radius: 6}
-
-    conn = RestClient::Resource.new("https://api.data.gov/nrel/alt-fuel-stations/v1", :headers => {'X-Api-Key' => ENV['nrel_api_key'], :params => params_hash})
-
-    response = conn['/nearest.json'].get
-
-    raw_stations = JSON.parse(response.body, symbolize_names: true)[:fuel_stations]
-
-    @stations = raw_stations.map do |raw_station|
-      Station.new(raw_station)
-    end
+  def conn(req_params) 
+    RestClient::Resource.new("https://api.data.gov/nrel/alt-fuel-stations/v1", :headers => {'X-Api-Key' => ENV['nrel_api_key'], :params => req_params})
   end
+
+  def req_params(zip) 
+    {fuel_type: 'ELEC,LPG', location: zip, limit: 10, radius: 6}
+  end
+
+  def fuel_stations(zip)
+    get_stations(parse_json(conn(req_params(zip))['/nearest.json'].get)[:fuel_stations])
+  end
+
+  private
+    def parse_json(response)
+      JSON.parse(response.body, symbolize_names: true)
+    end
+
+    def get_stations(raw_stations)
+      raw_stations.map { |raw_station| Station.new(raw_station) }
+    end
 end
